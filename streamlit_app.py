33666131e7c52783cb55d534505995358636e141
eleni <126977992+omiridoue@users.noreply.github.com>
2024-03-02 23:24:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,8 +265,8 @@ for index, d in enumerate(pyvis_graph.edges):
 for index, d in enumerate(pyvis_graph.edges):
     d['hidden'] = True  # Adds a new key 'title' with the value of 'label'
 
-     if d['title'] in ["1"]:
-        d.update((k, False) for k, v in d.items() if k == 'hidden')
+    if d['title'] in ["1"]:
+       d.update((k, False) for k, v in d.items() if k == 'hidden')
 
 
 for index, d in enumerate(pyvis_graph.nodes):
